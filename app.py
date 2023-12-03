4aa4d60df33bc021cac274e65ad9b12de793882e
Parikh oberoi <parikhoberoi330@gmail.com>
2023-12-03 08:19:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,12 +4,11 @@ from tempfile import NamedTemporaryFile
 import whisper
 import torch
 
-# Check if NVIDIA GPU is available
 torch.cuda.is_available()
 DEVICE = "cuda" if torch.cuda.is_available() else "cpu"
 
 # Load the Whisper model:
-model = whisper.load_model("base", device=DEVICE)
+model = whisper.load_model("medium", device=DEVICE)
 
 app = Flask(__name__)
 CORS(app)
